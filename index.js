7ed7e4402de5696f056c2b5990264c97d6a6a663
Zain Lateef <zain_lateef@wgresorts.com>
2019-09-25 11:41:34 -0400

    first draft complete

--- index.js
+++ index.js
@@ -2,28 +2,34 @@ var fs = require('fs');
 
 var modifiers, types 
 intializeKeywords();
-var textToConvert = fs.readFileSync('target.txt', 'utf8');
+var textToConvert = fs.readFileSync('input.txt', 'utf8');
 var linesOfText = textToConvert.split('\n');
 linesOfText = processLines(linesOfText);
 let result = '';
 linesOfText.forEach(line => result += convertLineToTypescript(line) + '\n');
-let test = fs.readFileSync('test.txt','utf8');
-if(test===result)
-    console.log("PASS")
-else
-    console.log("FAIL")
+fs.writeFileSync('output.txt',result);
+console.log("Done");
 
 function convertLineToTypescript(line) {
     let words = line.split(' ');
     let name = (words[2]).replace(';','');
     let type = types.get(words[1]);
-    if(!type) 
-        return arrayProtocol(name, words[1]);
+    if(!type) {
+        if(words[1].includes("Array") || words[1].includes("List"))
+            return arrayProtocol(name, words[1]);
+        else
+            return dtoProtocol(name, words[1]);
+    }
     else if(typeof words[4] !== 'undefined')
         return generateTypescriptDeclaration(name, type, words[4].replace(';',''));
     else
         return generateTypescriptDeclaration(name,type);
     
+}
+
+function dtoProtocol(name, dtoType) {
+    let type = dtoType.replace(/dto/gi, "");
+    return "@WGParam()\n" + generateTypescriptDeclaration(name, type);
 }
 
 function arrayProtocol(name, arrayType){
@@ -37,7 +43,7 @@ function arrayProtocol(name, arrayType){
     else {
         type = type.replace(/dto/gi, "");
         let arrayType = "Array<" + type + ">";
-        return generateTypescriptDeclaration(name,arrayType);
+        return "@WGParam()\n@WGList("+type+")\n" + generateTypescriptDeclaration(name,arrayType);
     }
 
 }
@@ -53,7 +59,8 @@ function processLines(lines){
     lines.forEach( (line, index) => lines[index] = line.replace(/[\t\n\r]/gm,'').trim() );
     lines = lines.filter( line => 
         line !== '' && 
-        line[0] != '@'
+        line[0] != '@' &&
+        line[0] != '/'
     );
     return lines;
 }
@@ -63,7 +70,3 @@ function intializeKeywords() {
     types = new Map([["Long", "number"], ["long", "number"], ["Integer", "number"], ["int","number"], 
     ["Double", "number"], ["double", "number"], ["String", "string"], ["Boolean", "boolean"], ["boolean","boolean"]]);
 }
-
-
-
-//line by line, parse and classify word or character
